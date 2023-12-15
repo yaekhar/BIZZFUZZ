f027b75c65c0d2cfb545388d22a17dbe67ad351e
yaekhar <yaekhar6958@gmail.com>
2023-12-15 13:57:46 +0900

    BIZZFIZZ

---
+++ buzzfizz.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("number: ");
+        int number = scanner.nextInt();
+
+        if (number % 5 == 0 && number % 3 == 0) {
+            System.out.println("BuzzFizz");
+        } else if (number % 5 == 0) {
+            System.out.println("Buzz");
+        } else if (number % 3 == 0) {
+            System.out.println("Fizz");
+        } else {
+            System.out.println("Invalid");
+        }
+    }
+}
